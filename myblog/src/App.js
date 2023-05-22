c2557fd04acacaa91d6a5357124f6b3e0c60b8ff
Holeey <izuodoh@yahoo.com>
2023-05-22 23:58:58 +0800

    imported browserRouter in indexjs

--- myblog/src/App.js
+++ myblog/src/App.js
@@ -4,7 +4,7 @@ import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
-  BrowserRouterProvider
+  BrowserRouter
 } from "react-router-dom";
 
 // components
@@ -28,7 +28,7 @@ const router = createBrowserRouter(
 function App() {
   return (
     <Fragment>
-      <BrowserRouterProvider router={router} />
+      <BrowserRouter router={router} />
     </Fragment>
   );
 }
