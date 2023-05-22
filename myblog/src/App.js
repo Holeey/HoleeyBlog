40303d57dc68820c8048ca8a526e5b02558625ee
Holeey <izuodoh@yahoo.com>
2023-05-22 10:20:44 +0800

    commit

--- myblog/src/App.js
+++ myblog/src/App.js
@@ -1,40 +1,35 @@
 import './App.css';
 import { Fragment } from 'react';
-
 import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
-  RouterProvider} from "react-router-dom";
+  BrowserRouterProvider
+} from "react-router-dom";
 
 // components
-
 import Navbar from './components/Navbar/Navbar';
 import HomePage from './components/HomePage/HomePage';
 import Portfolio from "./components/Portfolio/Portfolio";
 import About from './components/About/About';
 import Contact from './components/ContactPage/Contact';
 
-
 const router = createBrowserRouter(
   createRoutesFromElements(
-    
-    <Route path='/' element={<Navbar/>} >
-      <Route index element={<HomePage/>} />
+    <Route path="/" element={<Navbar />}>
+      <Route index element={<HomePage />} />
       <Route path="portfolio" element={<Portfolio />} />
-      <Route path='about' element={<About/>} />
-      <Route path='contact' element={<Contact />} />
+      <Route path="about" element={<About />} />
+      <Route path="contact" element={<Contact />} />
     </Route>
-   
   )
-)
-
+);
 
 function App() {
   return (
-   <Fragment>
-    <RouterProvider router={router} />
-   </Fragment>
+    <Fragment>
+      <BrowserRouterProvider router={router} />
+    </Fragment>
   );
 }
 
--- myblog/src/index.js
+++ myblog/src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { HashRouter as Router } from 'react-router-dom';
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <Router >
+ReactDOM.render(
+  <Router>
     <App />
   </Router>,
+  document.getElementById('root')
 );
+
 
 
