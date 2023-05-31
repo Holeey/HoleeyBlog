2e4c4b8103dd9fa57bf8ecc2ce6cfe15ac4c925c
Holeey <izuodoh@yahoo.com>
2023-06-01 06:06:45 +0800

    changed routerdom v6 to v5

--- myblog/src/App.js
+++ myblog/src/App.js
@@ -1,10 +1,44 @@
 import './App.css';
 import { Fragment } from 'react';
+import { BrowserRouter, Route, RouterProvider, Routes } from "react-router-dom";
+
+// components
+import Navbar from './components/Navbar/Navbar';
+import HomePage from './components/HomePage/HomePage';
+import Portfolio from "./components/Portfolio/Portfolio";
+import About from './components/About/About';
+import Contact from './components/ContactPage/Contact';
+
+const App = () => {
+  return (
+    <BrowserRouter basename="/holeeyblog">
+      <Fragment>
+        <Navbar />
+        <Routes>
+          <Route index element={<HomePage />} />
+          <Route path="portfolio" element={<Portfolio />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+        </Routes>
+      </Fragment>
+    </BrowserRouter>
+  );
+};
+
+export default App;
+
+
+
+
+
+/*import './App.css';
+import { Fragment } from 'react';
 import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
   RouterProvider,
+  Routes,
 } from "react-router-dom";
 
 // components
@@ -14,14 +48,14 @@ import Portfolio from "./components/Portfolio/Portfolio";
 import About from './components/About/About';
 import Contact from './components/ContactPage/Contact';
 
-const router = createBrowserRouter(
+const router = createBrowserRouter( {},
   createRoutesFromElements(
-    <Route path="/" element={<Navbar />}>
+    <Routes path="/" element={<Navbar />}>
       <Route index element={<HomePage />} />
       <Route path="portfolio" element={<Portfolio />} />
       <Route path="about" element={<About />} />
       <Route path="contact" element={<Contact />} />
-    </Route>
+    </Routes>
   )
 );
 
@@ -34,3 +68,4 @@ function App() {
 }
 
 export default App;
+*/
--- myblog/src/index.js
+++ myblog/src/index.js
@@ -2,18 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {
-  HashRouter
-  
-} from "react-router-dom";
+
 
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HashRouter >
+    
        <App />
-    </HashRouter>
+    
    
   </React.StrictMode>
 );
